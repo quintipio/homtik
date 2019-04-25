93752b2a0cde459f19cd5f0d1889f60784a4bcf7
Quentin Delfour <quentin.delfour.ext@orange.com>
2019-04-25 10:14:07 +0200

    Ajout de la gestion des ingrédients

---
+++ cuisine/ingredient/view_ingredient.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from cuisine.ingredient.form_ingredient import IngredientForm
+from cuisine.models import Ingredient
+
+
+@login_required
+def gestioningredient(request):
+    ingredients = Ingredient.objects.all().order_by("nom")
+    form_ajout = IngredientForm(request.POST or None)
+    return render(request, 'cuisine/gestionIngredients.html', locals())
+
+
+
+@login_required
+def ajouteringredient(request):
+    form = IngredientForm(request.POST or None)
+
+    if form.is_valid():
+        ingredients = form.save(commit=False)
+        ingredients.save()
+        form.save_m2m()
+        messages.success(request, "Ingrédient ajouté")
+    return redirect(gestioningredient)
+
--- cuisine/urls.py
+++ cuisine/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 from cuisine.autre_produit import view_autres_produits
+from cuisine.ingredient import view_ingredient
 
 urlpatterns = [
     path('accueil/', views.accueil, name="cuisineAccueil"),
     path('gestionProduits/', view_autres_produits.gestionproduits, name="gestionProduits"),
     path('gestionProduits', view_autres_produits.effacerproduit, name="effacerProduits"),
     path('gestionProduits/ajouter/', view_autres_produits.ajouterproduit, name="ajouter_produit"),
+    path('gestionIngredients/', view_ingredient.gestioningredient, name="gestionIngredient"),
+    path('gestionIngredients/ajouter/', view_ingredient.ajouteringredient, name="ajouter_ingredient"),
 ]
