d7dabbd8b475ef192f4958e1291e96c591de030b
Quentin Delfour <quentin.delfour.ext@orange.com>
2019-05-02 15:20:02 +0200

    Pour la Bdd

---
+++ init.sql
@@ -0,0 +1,8 @@
+INSERT INTO cuisine_unite (id, nom, quantite, diminutif, unite_mere_id) VALUES (1, 'Litre', 1, 'L', null);
+INSERT INTO cuisine_unite (id, nom, quantite, diminutif, unite_mere_id) VALUES (2, 'Kilo', 1, 'Kg', null);
+INSERT INTO cuisine_unite (id, nom, quantite, diminutif, unite_mere_id) VALUES (3, 'Unité', 1, 'u', null);
+INSERT INTO cuisine_unite (id, nom, quantite, diminutif, unite_mere_id) VALUES (4, 'Centilitre', 0.01, 'cL', 1);
+INSERT INTO cuisine_unite (id, nom, quantite, diminutif, unite_mere_id) VALUES (5, 'Mililitre', 0.001, 'mL', 1);
+INSERT INTO cuisine_unite (id, nom, quantite, diminutif, unite_mere_id) VALUES (6, 'Décilitre', 0.1, 'dL', 1);
+INSERT INTO cuisine_unite (id, nom, quantite, diminutif, unite_mere_id) VALUES (7, 'Gramme', 0.001, 'g', 2);
+INSERT INTO cuisine_unite (id, nom, quantite, diminutif, unite_mere_id) VALUES (8, 'Demi', 0, 'demi', 3);
