da1450f96ca91ec5befc7cfe900954d440cbbddd
Quentin Delfour <quentin.delfour.ext@orange.com>
2019-06-21 15:13:10 +0200

    Début de la apge de navigation

--- mediatheque/navigateur/view_nav.py
+++ mediatheque/navigateur/view_nav.py
@@ -1,5 +1,30 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
+import os
+
+FOLDER_ROOT = "/home/vnps3445/Public"
+
+
+def scan_folder(url: str):
+    list_files = {}
+    list_folder = {}
+    for element in os.listdir(FOLDER_ROOT+url):
+        if os.path.isdir(FOLDER_ROOT+url+element):
+            list_folder[url+element+"/"] = element
+        else:
+            list_files[url+element] = element
+    return list_files, list_folder
 
 
 def accueil(request):
-    return render(request, "mediatheque/accueil.html")
+    list_files, list_folder = scan_folder("/")
+    return render(request, "mediatheque/accueil.html", locals())
+
+
+def load_folder(request):
+    folder = request.POST['folder']
+    list_files, list_folder = scan_folder(folder)
+    data = {"list_files": list_files, "list_folder": list_folder}
+    return HttpResponse(json.dumps(data))
--- mediatheque/urls.py
+++ mediatheque/urls.py
@@ -4,5 +4,6 @@ from mediatheque.navigateur import view_nav
 
 urlpatterns = [
     path('navigateur/', view_nav.accueil, name="mediathequeAccueil"),
+    path('navigateur/path/', view_nav.load_folder, name="mediathequeNavigue"),
 
 ]
